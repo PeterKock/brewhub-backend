704024bc04abb42792e0b08391a0e86f51836f45
Peter Kock <ppkock@gmail.com>
2025-01-03 13:13:01 +0100

    Initial commit: Spring Boot project setup with base dependencies

---
+++ src/main/java/org/example/brewhubbackend/BrewhubBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.brewhubbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrewhubBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BrewhubBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/brewhubbackend/BrewhubBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.brewhubbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrewhubBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
